7b02d1916324b0382c6b29bc36f8c96fbcaeda73
JDIEvan <Forevanlee@gmail.com>
2026-03-03 21:42:11 -0800

    init Prisma Postgres mirgration to Railway

--- frontend/auth.ts
+++
@@ -1,8 +0,0 @@
-import NextAuth from "next-auth";
-import Google from "next-auth/providers/google";
-
-export const { handlers, auth, signIn, signOut } = NextAuth({
-  providers: [
-    Google, //Only using google for OAuth
-  ],
-});
