af24874a11b1ad5c76848fcf9114336c8dfba4a6
JDIEvan <110309927+Evan287@users.noreply.github.com>
2026-03-07 19:25:06 -0800

    Create Navbar plain text

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,7 +2,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
+
     <div>
+        <div className="Navbar">
+            <h3>Outstreamed</h3>
+            <h3>Leaderboards</h3>
+            <h3>About us</h3>
+        <\div>
       <h1>Outstreamed</h1>
       <h3>
         A fun game where you guess which spotify artist gets more monthly
